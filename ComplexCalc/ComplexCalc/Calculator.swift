e091b9a7036949d3414ad95a3d5a3f8e8e5507ce
Phillip Park <phillipypark@gmail.com>
2019-01-16 22:00:36 -0800

    All test work until count

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,44 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+    
+    func add(_ arr: [Int]) -> Int {
+        var sum = 0;
+        for index in arr {
+            sum+=arr[index]
+        }
+        return sum
+    }
+    
+    func multiply(_ arr: [Int]) -> Int {
+        var sum = 0;
+        for index in arr {
+            sum*=arr[index]
+        }
+        return sum
+    }
+    
+    func count(_ arr: [Int]) -> Int {
+        return arr.count
+    }
+    
 }
